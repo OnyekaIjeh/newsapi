0a7bc16e7963cdb32e4d50a3f3adf9895219c309
Onyeka Ijeh <onyekaijeh@yahoo.com>
2018-08-02 11:02:26 +0100

    change headlines to nigeria

--- src/store.js
+++ src/store.js
@@ -4,7 +4,9 @@ import Axios from 'axios'
 
 const Request = Axios.create({
   baseURL: 'https://newsapi.org/v2/',
-  headers: { 'X-Api-Key': '498755d8d73e49538e5fb46438797804' }
+  headers: {
+    'X-Api-Key': '498755d8d73e49538e5fb46438797804'
+  }
 })
 
 Vue.use(Vuex)
@@ -24,13 +26,16 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    getHeadlines({ commit, state }) {
+    getHeadlines({
+      commit,
+      state
+    }) {
       state.headlines = ''
       state.loading = true
       state.error = ''
       Request({
-        url: 'top-headlines?language=en'
-      })
+          url: 'top-headlines?language=en&country=ng'
+        })
         .then(data => {
           state.loading = false
           commit('setHeadlines', data.data.articles)
@@ -40,15 +45,18 @@ export default new Vuex.Store({
           state.error = error
         })
     },
-    getSearch({ commit, state }, payload) {
+    getSearch({
+      commit,
+      state
+    }, payload) {
       state.search = ''
       state.loading = true
       state.error = ''
       Request({
-        url: `everything?q=${payload.query}&sortBy=relevancy&page=${
+          url: `everything?q=${payload.query}&sortBy=relevancy&page=${
           payload.page
         }&language=en&pageSize=5`
-      })
+        })
         .then(data => {
           state.loading = false
           commit('setSearch', data.data.articles)
