b81f1014ce53a7894e12b079c93b3ffa7ce98082
Onyeka Ijeh <onyekaijeh@yahoo.com>
2018-08-03 14:19:16 +0100

    add reset.css

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router.js'
 import store from './store.js'
 import AppStory from './components/AppStory.vue'
 import Error from './components/Error.vue'
+import './assets/reset.css'
 
 Vue.component('app-story', AppStory)
 Vue.component('error', Error)
